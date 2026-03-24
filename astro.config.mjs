5c26857a3838035fd8baec82e99c2f328f517f09
kosei-okubo <koseiokubo@gmail.com>
2026-03-24 14:42:33 +0900

    Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel()
+  site: 'https://www.founderschecklist.ai',
+  adapter: vercel(),
+  integrations: [sitemap()]
 });
